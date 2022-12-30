2637e6a502fca7f4ba7e336a77c63871a68aab1e
Rob <houghton.robert.e@gmail.com>
2022-12-30 14:17:27 +0000

    Main trying to get this from codespaces to github

--- 8kyu_Python/Byte_Me.py
+++ 8kyu_Python/Byte_Me.py
@@ -1,3 +1,9 @@
+'''
+Return the size of an object in bytes. 
+Only the memory consumption directly attributed to the object is accounted for, not the memory consumption of objects it refers to.
+getsizeof() calls the objects __sizeof__ method and adds an additional garbage collector overhead if the object is managed by the garbage collector.
+'''
+
 import sys
 
 def total_bytes(obj): 
